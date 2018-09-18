15c5b1d2992a2e39d1890d0631cd959b931c36cb
xivk <ben@itinero.tech>
2018-09-18 09:52:39 +0200

    Get a proper overview of all worst cases.

--- test/Itinero.Tests.Functional/Program.cs
+++ test/Itinero.Tests.Functional/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Itinero.Data.Tiles;
 using Itinero.LocalGeo;
@@ -14,11 +15,11 @@ namespace Itinero.Tests.Functional
             {
                 Console.WriteLine(string.Format("[{0}-{3}] {1} - {2}", origin, level, message, DateTime.Now.ToString()));
             };
-            
-            int resolution = 4096;
-            uint zoom = 14;
 
-            var maxDistance = 0.0;
+            var resolutions = new int[] { 1024, 2048, 4096, 8192, 16384, 32768 };
+            var zooms = new uint[] {10, 11, 12, 13, 14 };
+            var worst = new Dictionary<(int resolution, uint zoom), double>();
+
             using (var stream = File.OpenRead(@"/home/xivk/work/data/OSM/belgium-latest.osm.pbf"))
             {
                 var source = new OsmSharp.Streams.PBFOsmStreamSource(stream);
@@ -28,21 +29,38 @@ namespace Itinero.Tests.Functional
                 {
                     if (!(osmGeo is Node node)) break;
 
-                    var tile = Tile.WorldToTile(node.Latitude.Value, node.Longitude.Value, zoom);
-                    var localCoordinates = tile.ToLocalCoordinates(node.Latitude.Value, node.Longitude.Value, resolution);
-                    var globalCoordinates = tile.FromLocalCoordinates(localCoordinates.x, localCoordinates.y, resolution);
-
-                    var distance = Coordinate.DistanceEstimateInMeter(node.Latitude.Value, node.Longitude.Value,
-                        globalCoordinates.latitude, globalCoordinates.longitude);
-
-                    if (distance > maxDistance)
+                    foreach (var zoom in zooms)
                     {
-                        maxDistance = distance;
-                        Console.WriteLine($"Max distance: {maxDistance}");
-                        File.AppendAllLines($"max-distance-{zoom}-{resolution}.csv", new string[] { $"{node.Id.Value};{node.Latitude.Value};" +
-                                                                                     $"{node.Longitude.Value},{distance}" });
+                        var tile = Tile.WorldToTile(node.Latitude.Value, node.Longitude.Value, zoom);
+                        foreach (var resolution in resolutions)
+                        {
+                            var current = (resolution, zoom);
+                            if (!worst.TryGetValue(current, out var currentWorst))
+                            {
+                                currentWorst = 0.0;
+                            }
+
+                            var localCoordinates =
+                                tile.ToLocalCoordinates(node.Latitude.Value, node.Longitude.Value, resolution);
+                            var globalCoordinates =
+                                tile.FromLocalCoordinates(localCoordinates.x, localCoordinates.y, resolution);
+
+                            var distance = Coordinate.DistanceEstimateInMeter(node.Latitude.Value, node.Longitude.Value,
+                                globalCoordinates.latitude, globalCoordinates.longitude);
+
+                            if (distance > currentWorst)
+                            {
+                                worst[current] = distance;
+                                Console.WriteLine($"New worst: {current}: {distance}");
+                            }
+                        }
                     }
                 }
+            }
+
+            foreach (var w in worst)
+            {
+                File.AppendAllLines($"worst.csv", new string[] { $"{w.Key.zoom};{w.Key.resolution};{w.Value}" });
             }
         }
     }
