2d6569cdff4692a2955a929d8f220491b4aba30f
xivk <ben@itinero.tech>
2020-03-11 13:24:14 +0100

    One more test storing shapes.

--- test/Itinero.Tests/Data/Graphs/GraphTileTests.cs
+++ test/Itinero.Tests/Data/Graphs/GraphTileTests.cs
@@ -102,5 +102,43 @@ namespace Itinero.Tests.Data.Graphs
             Assert.Equal(4.86786, shapePoint.longitude, 4);
             Assert.Equal(51.26909, shapePoint.latitude, 4);
         }
+
+        [Fact]
+        public void GraphTile_AddEdge0_ThreeShapePoint_ShouldStoreThreeShapePoints()
+        {
+            var graphTile = new GraphTile(14,
+                Tile.WorldToTile(4.86638, 51.269728, 14).LocalId);
+            var vertex1 = graphTile.AddVertex(4.86638, 51.269728);
+            var vertex2 = graphTile.AddVertex(4.86737, 51.267849);
+
+            var edge = graphTile.AddEdge(vertex1, vertex2, new[]
+            {
+                (
+                    4.867324233055115,
+                    51.269695361396586
+                ),
+                (
+                    4.867860674858093,
+                    51.26909794023487
+                ),
+                (
+                    4.868037700653076,
+                    51.26838639478469
+                )
+            });
+
+            var shapes = graphTile.GetShape(edge).ToList();
+            Assert.NotNull(shapes);
+            Assert.Equal(3, shapes.Count);
+            var shapePoint = shapes[0];
+            Assert.Equal(4.867324233055115, shapePoint.longitude, 4);
+            Assert.Equal(51.269695361396586, shapePoint.latitude, 4);
+            shapePoint = shapes[1];
+            Assert.Equal(4.867860674858093, shapePoint.longitude, 4);
+            Assert.Equal(51.26909794023487, shapePoint.latitude, 4);
+            shapePoint = shapes[2];
+            Assert.Equal(4.868037700653076, shapePoint.longitude, 4);
+            Assert.Equal(51.26838639478469, shapePoint.latitude, 4);
+        }
     }
 }
