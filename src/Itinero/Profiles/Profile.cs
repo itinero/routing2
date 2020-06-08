c22c897312c17718877c7c4f056e00393a8b9b67
Pieter Vander Vennet <pietervdvn@gmail.com>
2020-06-08 17:07:56 +0200

    Add a small comment

--- src/Itinero/Profiles/Profile.cs
+++ src/Itinero/Profiles/Profile.cs
@@ -5,6 +5,9 @@ namespace Itinero.Profiles
     /// <summary>
     /// Abstract representation of a profile.
     /// </summary>
+    /// <remarks>
+    /// This is a single possible behaviour of a vehicle (e.g. 'bicycle.fastest').
+    /// </remarks>
     public abstract class Profile
     {
         /// <summary>
