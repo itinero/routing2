5ee1d512d1b6cef4bfba3a8b4a1ce03b3e6bf893
Pieter Vander Vennet <pietervdvn@gmail.com>
2020-06-18 17:12:46 +0200

    Create project for instruction generation

---
+++ src/Itinero.Instructions/README.md
@@ -0,0 +1,3 @@
+# Instruction generation
+
+This project focusses on the generation of instructions for a given route.
---
+++ test/Itinero.Tests/Instructions/InstructionTest.cs
@@ -0,0 +1,19 @@
+using Itinero.Algorithms.Routes;
+using Xunit;
+
+namespace Itinero.Tests.Instructions
+{
+    public class InstructionTest
+    {
+
+        [Fact]
+        public void Test1()
+        {
+
+            var route = new RouteBuilder();
+            Assert.False(true);
+
+        }
+        
+    }
+}
