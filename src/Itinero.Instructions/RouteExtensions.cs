992e51ccb840e00ee973cc9691a0245f528eade3
pietervdvn <pietervdvn@posteo.net>
2021-03-29 15:57:28 +0200

    Expose a method to calculate bearing between two points

--- src/Itinero.Instructions/RouteExtensions.cs
+++ src/Itinero.Instructions/RouteExtensions.cs
@@ -12,6 +12,7 @@ namespace Itinero.Instructions
         {
             return new(route, generator);
         }
+
         /// <summary>
         ///     Adds the default instructions to the route,
         /// </summary>
@@ -29,6 +30,25 @@ namespace Itinero.Instructions
             }
 
             return sum;
+        }
+
+        /// <summary>
+        ///     Gives the absolute bearing if travelling from the given shapeindex towards the next shapeIndex.
+        /// </summary>
+        /// <remarks>0° is north, 90° is east, -90° is west, both 180 and -180 are south. Gives null for the last point</remarks>
+        /// <param name="route"></param>
+        /// <param name="shape"></param>
+        /// <returns></returns>
+        public static double? BearingAt(this Route route, int shape)
+        {
+            if (route.Shape.Count == shape + 2) { // Plus two, as we'll increase shape later on
+                return null;
+            }
+
+            var current = route.Shape[shape];
+            var next = route.Shape[shape + 1];
+
+            return Utils.AngleBetween(current, next);
         }
     }
 }
