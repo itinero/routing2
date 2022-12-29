cae9a26395a8b8b154fc50ce580db335fa3a1e0b
xivk <ben@anyways.eu>
2022-12-29 13:59:54 +0100

    Bumped version #.

---
+++ src/Itinero.Geo/AttributesTableExtensions.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using NetTopologySuite.Features;
+
+namespace Itinero.Geo;
+
+/// <summary>
+/// Extension methods related to attributes tables
+/// </summary>
+public static class AttributesTableExtensions
+{
+    /// <summary>
+    /// Converts the given attributes table to attributes for Itinero.
+    /// </summary>
+    /// <param name="attributesTable">The attributes table.</param>
+    /// <returns>Attributes as string key-value pairs.</returns>
+    public static IEnumerable<(string key, string value)> ToAttributes(this IAttributesTable attributesTable)
+    {
+        var names = attributesTable.GetNames();
+        foreach (var name in names)
+        {
+            yield return (name, attributesTable[name].ToInvariantString());
+        }
+    }
+        
+    /// <summary>
+    /// Returns a string representing the object in a culture invariant way.
+    /// </summary>
+    private static string ToInvariantString(this object obj)
+    {
+        return obj switch
+        {
+            IConvertible convertible => convertible.ToString(CultureInfo.InvariantCulture),
+            IFormattable formattable => formattable.ToString(null, CultureInfo.InvariantCulture),
+            _ => obj.ToString()
+        };
+    }
+}
---
+++ src/Itinero.Geo/CoordinateExtensions.cs
@@ -0,0 +1,30 @@
+using NetTopologySuite.Geometries;
+
+namespace Itinero.Geo;
+
+/// <summary>
+/// Extension methods related to coordinates.
+/// </summary>
+public static class CoordinateExtensions
+{
+    /// <summary>
+    /// Converts the coordinate to an Itinero coordinate tuple.
+    /// </summary>
+    /// <param name="coordinate">The coordinate.</param>
+    /// <returns>A coordinate tuple.</returns>
+    public static (double longitude, double latitude, float? e) ToCoordinateTuple(this Coordinate coordinate)
+    {
+        return (coordinate.X, coordinate.Y, null);
+    }
+    
+    /// <summary>
+    /// Returns an estimate of the distance between the two given coordinates.
+    /// </summary>
+    /// <param name="coordinate">The first coordinate.</param>
+    /// <param name="other">The second coordinate.</param>
+    /// <remarks>Accuracy decreases with distance.</remarks>
+    public static double DistanceEstimateInMeter(this Coordinate coordinate, Coordinate other)
+    {
+        return coordinate.ToCoordinateTuple().DistanceEstimateInMeter(other.ToCoordinateTuple());
+    }
+}
