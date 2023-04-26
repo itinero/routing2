5381a3e8a1fd025a8e0ec4f84a6d098c2a925f0e
xivk <ben@anyways.eu>
2023-04-26 10:12:41 +0200

    Fixed use of cancellation token in routing network query.

--- src/Itinero/Search/RoutingNetworkQuery.cs
+++ src/Itinero/Search/RoutingNetworkQuery.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using Itinero.Network;
 using Itinero.Network.Search;
 
@@ -12,16 +14,17 @@ internal class RoutingNetworkQuery : IRoutingNetworkQuery
     {
         _network = network;
     }
-
     public async IAsyncEnumerable<VertexId> Vertices(
         ((double longitude, double latitude, float? e) topLeft, (double longitude, double latitude, float? e) bottomRight)
-            box)
+            box, [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        await _network.UsageNotifier.NotifyBox(_network, box);
-
+        await _network.UsageNotifier.NotifyBox(_network, box, cancellationToken);
+        if (cancellationToken.IsCancellationRequested) yield break;
+        
         var vertices = _network.SearchVerticesInBox(box);
         foreach (var (vertex, _) in vertices)
         {
+            if (cancellationToken.IsCancellationRequested) yield break;
             yield return vertex;
         }
     }
